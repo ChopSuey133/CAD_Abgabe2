43b0a5f34e583b423d777f3f93147feb5f29c962
ChopSuey133 <Hubert.schwaiger@gmx.net>
2024-05-16 20:13:40 +0200

    Fertige Abgabe
    Abgabe ist fertig

--- appSettings.js
+++ appSettings.js
@@ -1,10 +1,13 @@
 const settings = {
-    'clientId': 'YOUR_CLIENT_ID_HERE',
-    'tenantId': 'common',
+    //'clientId': '70226e87-4bc0-44b5-8bf8-e31ce9584119',       //Tenant 1 MSFT
+    //'tenantId': '31128ea6-61a9-4baf-8a3e-2e50e7878a5e',
+    'clientId': 'fe8f7cfc-de89-4dcc-82eb-aaaf76c221bc',         //Tenant 2 MSFT
+    'tenantId': 'd261c78a-c566-4d91-a19c-bb838c9dbbcc',
     'graphUserScopes': [
       'user.read',
       'mail.read',
-      'mail.send'
+      'mail.send',
+      'calender.read'
     ]
   };
   
